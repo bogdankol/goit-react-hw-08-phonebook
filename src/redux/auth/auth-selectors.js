1595bd709ac5818420d1ce2730ecbf10e17b9965
bogdankol <bogdank208@gmail.com>
2021-10-15 15:07:37 +0300

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux";
 
-const getIsLoggedIn = (state) => state.auth.isLoggedIn;
+const getIsLoggedIn = (state) => {
+
+  return state.auth.token ? true : false;
+};
 const getName = (state) => state.auth.user.name;
 const getIsFetchingUser = (state) => state.auth.isFetchingUser;
 
